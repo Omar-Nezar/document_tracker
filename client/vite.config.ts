f751cf7012baa33450edba4147ad4c066695115d
Omar Nezar <frcollege2@gmail.com>
2026-08-12 12:09:14 +0400

    Included qr code plugin + host option for quick mobile testing

---
+++ client/vite.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite'
+import react, { reactCompilerPreset } from '@vitejs/plugin-react'
+import babel from '@rolldown/plugin-babel'
+import tailwindcss from '@tailwindcss/vite'
+import { qrcode } from "vite-plugin-qrcode"
+import path from 'path'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    babel({ presets: [reactCompilerPreset()] }),
+    tailwindcss(),
+    qrcode()
+  ],
+  server: {
+    host: true, // For testing on mobile with qr code
+    port: 5173,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(import.meta.dirname, "./"),
+    }
+  }
+})
