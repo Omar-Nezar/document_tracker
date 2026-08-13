8be7c094022a3b543b3a91965b9e101fd9802595
Omar Nezar <frcollege2@gmail.com>
2026-08-13 12:51:03 +0400

    Added some initial routes with fetch user functionality to test

---
+++ server/src/controllers/admin.controller.ts
@@ -0,0 +1,16 @@
+import type { Request, Response } from "express";
+import { db } from "../db";
+
+export const getUsers = async (req: Request, res: Response) => {
+    try {
+        const users = await db.query.usersTable.findMany({
+            columns: {
+                password: false, // Explicitly exclude password column
+            },
+        });
+
+        return res.status(200).json({ users, message: "Users fetched successfully" });
+    } catch (error) {
+        return res.status(500).json({ message: "Internal server error" })
+    }
+};
