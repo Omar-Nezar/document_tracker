b826508465c0103bde619290d83e72e8d00af033
Omar Nezar <frcollege2@gmail.com>
2026-08-13 12:50:40 +0400

    Initial server with demo function to add, update and remove from postgres server

---
+++ server/src/server.ts
@@ -0,0 +1,60 @@
+import 'dotenv/config';
+import express, { type Request, type Response } from 'express';
+import cors from "cors"
+
+import { eq } from 'drizzle-orm';
+import { db } from './db';
+import * as schema from "./db/schema";
+
+import adminRoutes from "./routes/admin.routes"
+
+const app = express();
+app.use(express.json());
+
+app.use(
+    cors({
+        // origin: "http://localhost:5173", // frontend URL
+        // credentials: true, to be enabled later for security
+    })
+);
+
+const port = process.env.PORT
+app.listen(port, () => {
+    console.log(`Server running on ${port}`);
+});
+
+app.get("/", (req: Request, res: Response) => {
+    res.send("API is running...");
+});
+
+app.use("/admin", adminRoutes)
+
+/*
+ *  Sample script demo
+ */
+async function main() {
+    const user: typeof schema.usersTable.$inferInsert = {
+        name: 'John',
+        email: 'john@example.com',
+        password: "password"
+    };
+
+    await db.insert(schema.usersTable).values(user);
+    console.log('New user created!')
+
+    const users = await db.select().from(schema.usersTable);
+    console.log('Getting all users from the database: ', users)
+
+    await db
+        .update(schema.usersTable)
+        .set({
+            name: 'Jonathan',
+        })
+        .where(eq(schema.usersTable.email, user.email));
+    console.log('User info updated!')
+
+    await db.delete(schema.usersTable).where(eq(schema.usersTable.email, user.email));
+    console.log('User deleted!')
+}
+
+main();
